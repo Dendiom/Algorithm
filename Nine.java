cf99f4224e8e85b3e60684595ae1a95eb989b542
yuanpu <542959871@qq.com>
2017-03-25 10:36:09 +0800

    ok

--- Nine.java
+++ Nine.java
@@ -2,4 +2,33 @@ package TestOne;
 
 
 public class Nine {
+
+
+//from leetcode  ,so good
+  public static boolean isPalindrome(int x) {
+
+    if (x < 0) return false;
+
+    int p = x;
+    int q = 0;
+
+    while (p >= 10){
+        q *=10;
+        q += p%10;
+        p /=10;
+    }
+
+    return q == x / 10 && p == x % 10;
+}
+
+
+    public static void main(String[] args) {
+        System.out.println("ans" + isPalindrome(1000021));
+        System.out.println("ans" + isPalindrome(1232321));
+        System.out.println("ans" + isPalindrome(123321));
+        System.out.println("ans" + isPalindrome(-1345));
+
+    }
+
+
 }
