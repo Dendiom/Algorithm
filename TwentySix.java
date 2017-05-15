a0640f1be44b186ae2bf8753f06f583c9ce355f8
yuanpu <542959871@qq.com>
2017-05-15 20:38:39 +0800

    delete duplicates in array

---
+++ TwentySix.java
@@ -0,0 +1,24 @@
+package TestOne;
+
+
+public class TwentySix {
+
+    public static int deleteDuplicates(int[] nums){
+        int lens = nums.length,ans = 1;
+        if(lens<2)
+            return lens;
+        for(int i = 0;i<lens-1;i++){
+            if(nums[i] != nums[i+1]) {
+                nums[ans] = nums[i+1];
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        System.out.println(deleteDuplicates(new int[]{1,1,2,2,3,5,5,5,7}));
+        System.out.println(deleteDuplicates(new int[]{1,1,1,1,3}));
+        System.out.println(deleteDuplicates(new int[]{1}));
+    }
+}
