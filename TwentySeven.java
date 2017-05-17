821fa2fd24f06506527cc7875fb836c48af4cc26
yuanpu <542959871@qq.com>
2017-05-17 11:42:14 +0800

    delete element

---
+++ TwentySeven.java
@@ -0,0 +1,21 @@
+package TestOne;
+
+
+public class TwentySeven {
+
+    public static int deleteElement(int[] nums,int val){
+        int ans = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] != val){
+                nums[ans] = nums[i];
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println(deleteElement(new int[]{3,2,2,4,5},3));
+    }
+}
