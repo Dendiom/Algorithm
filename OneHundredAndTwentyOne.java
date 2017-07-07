1fb2e8fd3d306e6019091c0d7cc384ddfbb7b270
yuanpu <542959871@qq.com>
2017-07-07 10:22:45 +0800

    best time to sell stock

---
+++ OneHundredAndTwentyOne.java
@@ -0,0 +1,26 @@
+package TestOne;
+
+
+public class OneHundredAndTwentyOne {
+
+    public static int maxPrice(int[] nums){
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int maxProfit = 0;
+        int minPrice = nums[0];
+        for(int i =1;i<nums.length;i++){
+            minPrice = Math.min(minPrice, nums[i]);
+            maxProfit = Math.max(nums[i] - minPrice,maxProfit);
+        }
+        return maxProfit > 0?maxProfit:0;
+    }
+
+    public static void main(String[] args){
+        System.out.println(maxPrice(new int[]{1,2,7,4,3,4}));
+        System.out.println(maxPrice(new int[]{5,2,9,4,3,7}));
+        System.out.println(maxPrice(new int[]{5,2,7,2,3,4}));
+        System.out.println(maxPrice(new int[]{5,1,7,2,3,8}));
+        System.out.println(maxPrice(new int[]{6,5,3,2,1}));
+    }
+}
