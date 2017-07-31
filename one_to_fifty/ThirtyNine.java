46090b62fc7f3497a8dda00eab0dbf81a84d531c
yuanpu <542959871@qq.com>
2017-07-31 23:06:29 +0800

    combination sum

---
+++ one_to_fifty/ThirtyNine.java
@@ -0,0 +1,36 @@
+package TestOne.one_to_fifty;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThirtyNine {
+	
+	//backtrack, target and candidates are positive integer  is very important.
+
+	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+		List<List<Integer>> ans = new ArrayList<>();
+		Arrays.sort(candidates);
+		backtrack(ans, new ArrayList<>(), candidates, target, 0);
+		return ans;
+	}
+	
+	private static void backtrack(List<List<Integer>> ans, List<Integer> temp, int[] candidates, int target, int start){
+		if (target < 0)
+			return;
+		if (target == 0)
+			ans.add(new ArrayList<>(temp));
+		else {
+			for (int i = start; i < candidates.length; i++) {
+				temp.add(candidates[i]);
+				backtrack(ans, temp, candidates, target - candidates[i], i);
+				temp.remove(temp.size() - 1);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(combinationSum(new int[]{1,4,5,2,3}, 4));
+	}
+
+}
