22b17d162098222a58591cebca35de147c43340a
yuanpu <542959871@qq.com>
2017-10-07 21:00:16 +0800

    obstacle unique path

---
+++ fifty_to_hundred/SixtyThree.java
@@ -0,0 +1,48 @@
+package TestOne.fifty_to_hundred;
+
+import java.util.Arrays;
+
+public class SixtyThree {
+	
+	// still easy dp
+	public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		int m = obstacleGrid.length;
+		if (m == 0)
+			return 0;
+		int n = obstacleGrid[0].length;
+		if (n == 0)
+			return 0;
+
+		int[][] path = new int[m][n];
+		for (int i = 0; i < n; i++) {
+			if (obstacleGrid[0][i] == 0) {
+				path[0][i] = 1;
+			} else {
+				break;
+			}
+		}
+		for (int i = 0; i < m; i++) {
+			if (obstacleGrid[i][0] == 0) {
+				path[i][0] = 1;
+			} else {
+				break;
+			}
+		}
+
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				if (obstacleGrid[i][j] == 1) {
+					path[i][j] = 0;
+				} else {
+					path[i][j] = path[i - 1][j] + path[i][j -1];
+				}
+			}
+		}
+		return path[m - 1][n - 1];
+	}
+
+	public static void main(String[] args) {
+		int[][] path = new int[3][2];
+		System.out.println(Arrays.toString(path[0]));
+	}
+}
