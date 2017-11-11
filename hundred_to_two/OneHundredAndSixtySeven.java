a2b01dc4cfc3a0384acfca9f5c81bccc840d9608
yuanpu <542959871@qq.com>
2017-11-11 20:56:53 +0800

    two sum -- sorted set

---
+++ hundred_to_two/OneHundredAndSixtySeven.java
@@ -0,0 +1,33 @@
+package TestOne.hundred_to_two;
+
+import java.util.Arrays;
+
+public class OneHundredAndSixtySeven {
+
+	/**
+	 * array is sorted, so we can search from border to middle.
+	 */
+	public static int[] twoSum(int[] numbers, int target) {
+		int[] ans = new int[2];    // question detail says that there must be an answer
+		int start = 0;
+		int end = numbers.length - 1;
+		while (true) {
+			if (numbers[start] + numbers[end] == target) {
+				ans[0] = start + 1;
+				ans[1] = end + 1;
+				return ans;
+			} else if (numbers[start] + numbers[end] > target) {
+				end--;
+			} else {
+				start++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(twoSum(new int[] { 1, 2 }, 3)));
+		System.out.println(Arrays.toString(twoSum(new int[] { 1, 2, 7, 9 }, 9)));
+		System.out.println(Arrays.toString(twoSum(new int[] { 1, 5, 6, 12, 15 }, 17)));
+	}
+
+}
