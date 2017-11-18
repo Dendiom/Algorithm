d8c8163cd439531e281ed34e113b8f23a4826ba0
yuanpu <542959871@qq.com>
2017-11-18 17:45:50 +0800

    swap paires

--- one_to_fifty/TwentyFour.java
+++ one_to_fifty/TwentyFour.java
@@ -1,31 +1,44 @@
 package TestOne.one_to_fifty;
 
-
 public class TwentyFour {
 
-    public class ListNode {
-        int val;
-        ListNode next;
+	public static class ListNode {
+		int val;
+		ListNode next;
 
-        ListNode(int x) {
-            val = x;
-        }
-    }
+		ListNode(int x) {
+			val = x;
+		}
+	}
 
-    public ListNode swapPairs(ListNode head) {
-        ListNode fakeHead = new ListNode(0);
-        ListNode pre = fakeHead;
-        ListNode copy = null;
-        while(head != null && head.next != null) {
-            copy = head;
-            head = head.next.next;
-            pre.next = copy.next;
-            pre.next.next = copy;
-            pre = copy;
-        }
-        if (head != null) { // head.next == null
-            pre.next = head;
-        }
-        return fakeHead.next;
-    }
+	public static ListNode swapPairs(ListNode head) {
+		ListNode fakeHead = new ListNode(0);
+		ListNode pre = fakeHead;
+		ListNode copy = null;
+		while (head != null && head.next != null) {
+			copy = head;
+			head = head.next.next;
+			pre.next = copy.next;
+			pre.next.next = copy;
+			pre = copy;
+			pre.next = null;      // very important, or will be a circle
+		}
+		if (head != null) { // head.next == null
+			pre.next = head;
+		}
+		return fakeHead.next;
+	}
+
+	public static void main(String[] args) {
+		ListNode nodeOne = new ListNode(0);
+		ListNode nodeTwo = new ListNode(1);
+		ListNode nodeThree = new ListNode(2);
+		nodeOne.next = nodeTwo;
+		//nodeTwo.next = nodeThree;
+		ListNode s = swapPairs(nodeOne);
+		while (s != null) {
+			System.out.println(s.val);
+			s = s.next;
+		}
+	}
 }
