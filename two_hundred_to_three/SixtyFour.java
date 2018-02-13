18e2cc3097eb2405aa483430e43b7bcf0810f837
yuanpu <542959871@qq.com>
2018-02-13 19:23:24 +0800

    nth ugly number II

--- two_hundred_to_three/SixtyFour.java
+++ two_hundred_to_three/SixtyFour.java
@@ -1,15 +1,44 @@
 package LeetCode.two_hundred_to_three;
 
+
 public class SixtyFour {
 
+    /**
+     * Solution from leetcode.
+     * ugly[n] = min(2 * ugly[i], 3 * ugly[j], 5 * ugly[k]). If 2 * ugly[i] is the minimum,
+     * ugly[n + 1] = min(2 * ugly[i + 1], 3 * ugly[j], 5 * ugly[k]).
+     */
     private static int nthUglyNumber(int n) {
+        if (n == 0) {
+            return 1;
+        }
 
-        return 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int index2 = 0, index3 = 0, index5 = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.min(2 * dp[index2], Math.min(3 * dp[index3], 5 * dp[index5]));
+            System.out.println(dp[i]);
+            if (dp[i] == 2 * dp[index2]) {
+                index2++;
+            }
+
+            if (dp[i] == 3 * dp[index3]) {
+                index3++;
+            }
+
+            if (dp[i] == 5 * dp[index5]) {
+                index5++;
+            }
+        }
+
+        return dp[n - 1];
     }
 
 
-
     public static void main(String[] args) {
-        System.out.println(nthUglyNumber(6));
+        //System.out.println(nthUglyNumber(6));
+        //System.out.println(nthUglyNumber(7));
+        System.out.println(nthUglyNumber(8));
     }
 }
