71ee0a9af80e4104ac90979ada9e9c5592db162f
yuanpu <542959871@qq.com>
2018-02-17 10:37:31 +0800

    same tree

---
+++ fifty_to_hundred/OneHundred.java
@@ -0,0 +1,35 @@
+package LeetCode.fifty_to_hundred;
+
+public class OneHundred {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * My approach:
+     *  Inorder traversal.
+     */
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
