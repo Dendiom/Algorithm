c2b650902df4bed553d34ff6d9463c412edcd1cb
yuanpu <542959871@qq.com>
2018-04-08 15:57:17 +0800

    minium depth of binary tree

---
+++ hundred_to_two/Eleven.java
@@ -0,0 +1,30 @@
+package LeetCode.hundred_to_two;
+
+public class Eleven {
+
+    private final class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        if (left == 0 || right == 0) {
+            return left + right + 1;
+        }
+
+        return 1 + Math.min(left, right);
+    }
+}
